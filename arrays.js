f496d466ca5a4fb55f4d565c6ed539dd5262c8f6
Michelle B <mjblevins7@gmail.com>
2017-11-07 02:55:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,17 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(element, array) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray (array, index) {
+  array[index];
+  return array;
+}
