219411191949ad02c674a4b91a55676bc1842f7e
Michelle B <mjblevins7@gmail.com>
2017-11-07 02:52:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,8 @@ var chocolateBars = ['snickers','hundred grand','kit kat', 'skittles'];
 function addElementToBeginningOfArray(element, array) {
   return [element, ...array];
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
-array.unshift(element);
-return array;
+  array.unshift(element);
+  return array;
 }
