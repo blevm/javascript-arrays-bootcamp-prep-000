ec231d9e0b328dcb5b5930da79c840734642ecb4
Michelle B <mjblevins7@gmail.com>
2017-11-07 02:45:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers','hundred grand','kit kat', 'skittles'];
 
 function addElementToBeginningOfArray(element, array) {
-  array.push(element);
+  return [element, ...array];
 }
